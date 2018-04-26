6c9da47e322340027e8413d31d5c3ea1828e98ce
Harshith <Harshith>
2018-04-26 21:19:19 +0530

    Commit Final

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 var app = angular.module("chat-app",[]);
 app.controller("chat-controller",function($scope, $http) {
     $scope.selectValue = "None";
-    $http.get('https://chat-with-harshi.herokuapp.com/retrieve')
-    //$http.get('http://localhost:3000/retrieve')
+    //$http.get('https://chat-with-harshi.herokuapp.com/retrieve')
+    $http.get('http://localhost:3000/retrieve')
 		.then(function(response){
             $scope.roomsList = response.data;
 		},function(error) {
