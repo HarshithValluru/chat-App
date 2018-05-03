31d6ac76a8c48f3796d2be06686d5b2c1f56850b
V Harshith <v.harshith@ad.infosys.com>
2018-05-03 18:18:28 +0530

    Emojis to be added

--- public/js/chat.js
+++ public/js/chat.js
@@ -19,6 +19,7 @@ function scrollToBottom() {
 socket.on("connect",function() {
     console.log("Connected to Server..");
     var params = jQuery.deparam(window.location.search);
+    document.getElementById("loggedUser").innerHTML=params.name;
     if(params.availRooms === "None")
         params.room = params.newRoom;
     else
@@ -99,3 +100,15 @@ locButton.on("click",function() {
 socket.on("disconnect",function() {
     console.log("Disconnected from the Server..");
 });
+//$(function() {
+  // Initializes and creates emoji set from sprite sheet
+  window.emojiPicker = new EmojiPicker({
+    emojiable_selector: '[data-emojiable=true]',
+    assetsPath: './../img/',
+    popupButtonClasses: 'fa fa-smile-o'
+  });
+  // Finds all elements with `emojiable_selector` and converts them to rich emoji input fields
+  // You may want to delay this step if you have dynamically created input fields that appear later in the loading process
+  // It can be called as many times as necessary; previously converted input fields will not be converted again
+  window.emojiPicker.discover();
+//});
